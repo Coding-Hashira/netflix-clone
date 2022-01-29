467c224e1845d0378fdd7cebda6044c59027104c
DevOM <oplondhe@gmail.com>
2022-01-29 19:09:50 +0530

    After solving trailer issue

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
-import React, {useState} from 'react';
-import './App.css';
-import Row from './Components/Row';
-import {requests} from './requests';
-import Banner from './Components/Banner'
-import Navbar from './Components/Navbar';
+import React, { useState } from "react";
+import "./App.css";
+import Row from "./Components/Row";
+import { requests } from "./requests";
+import Banner from "./Components/Banner";
+import Navbar from "./Components/Navbar";
+import YouTube from "react-youtube";
 
 function App() {
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const [movieID, setMovieID] = useState("");
 
   return (
     <div className="App">
-        <Navbar/>
+      <Navbar />
 
-        <Banner/>
-        <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLarge={true}/>
-        <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-        <Row title="Top Action" fetchUrl={requests.fetchAction}/>
-        <Row title="Top Adventure" fetchUrl={requests.fetchAdventure}/>
-        <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
-        <Row title="Top Comedy" fetchUrl={requests.fetchComedy}/>
-        <Row title="Top Horror" fetchUrl={requests.fetchHorror}/>
+      <Banner />
+      <Row
+        title="Trending Now"
+        fetchUrl={requests.fetchTrending}
+        isLarge={true}
+        trailerUrl={trailerUrl}
+        setTrailerUrl={setTrailerUrl}
+        movieID={movieID}
+        setMovieID={setMovieID}
+      />
+      {trailerUrl && (
+        <YouTube
+          className="video"
+          videoId={trailerUrl}
+          opts={{
+            height: "750",
+            width: "100%",
+            playerVars: {
+              // https://developers.google.com/youtube/player_parameters
+              autoplay: 1,
+              origin: "https://www.youtube.com/embed/",
+            },
+          }}
+        />
+      )}
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Top Action" fetchUrl={requests.fetchAction} />
+      <Row title="Top Adventure" fetchUrl={requests.fetchAdventure} />
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Top Comedy" fetchUrl={requests.fetchComedy} />
+      <Row title="Top Horror" fetchUrl={requests.fetchHorror} />
     </div>
-    )
-
+  );
 }
 
 export default App;
